c3bc59148a842757cbf82b36688496494ec27c2d
marsannar2 <soymarcosr@gmail.com>
2025-07-01 13:56:10 +0200

    añadido procesamiento de transacciones

--- transaction/Transaction.java
+++ transaction/Transaction.java
@@ -1,10 +1,11 @@
 package transaction;
-
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.ArrayList;
+import java.util.List;
 
 import utils.StringUtils;
+import main.BloqueChain;
 
 public class Transaction {
 
@@ -52,6 +53,67 @@ public class Transaction {
     public boolean verifySignature() throws Exception{
         String data = StringUtils.getStringFromKey(receiverKey) + StringUtils.getStringFromKey(senderKey) + Double.toString(value);
 	    return StringUtils.verifyECDSASignature(signature,senderKey,data);
+    }
+
+    public void processTransaction() throws Exception{
+        if(verifySignature() == false){
+            throw new Exception("The transaction signature does not match with the one expected");
+        }
+
+        // recuperamos las entradas de transacciones que todavía no han sido gastadas 
+        for(TransactionInput input: inputs){
+            input.setUTXO(BloqueChain.UnspentTransactionOutputs.get(input.getUTXO().getId()));;
+        }
+
+        Double remaining = getInputsValue() - value;
+        this.hash = createHash();
+        // se envía la cantidad establecida al receptor y se registra ese cambio en el remitente
+        outputs.add(new TransactionOutput(hash, value, this.receiverKey));
+        outputs.add(new TransactionOutput(hash, remaining, this.senderKey));
+
+        // se registran estas salidas en la red de bloques
+        for(TransactionOutput output: outputs){
+            if(!BloqueChain.UnspentTransactionOutputs.containsKey(output.getId())){
+                BloqueChain.UnspentTransactionOutputs.put(output.getId(),output);
+            } 
+        }
+
+        //vaciamos la memoria de las salidas sin gastar de la red de bloques
+        for(TransactionInput input : inputs){
+            if(input.getUTXO() == null) continue;
+            BloqueChain.UnspentTransactionOutputs.remove(input.getUTXO().getId());
+        }
+
+    }
+
+    public Double getInputsValue(){
+        Double inputsValue = 0.0;
+        for(TransactionInput input : inputs){
+            if(input.getUTXO() == null) continue;
+            inputsValue += input.getUTXO().getAmount();
+        }
+        return inputsValue;
+    }
+
+    public Double getOutputsValue(){
+        Double OutputsValue = 0.0;
+        for(TransactionOutput output : outputs){
+            //Solo escogemos 
+            OutputsValue += output.getAmount();
+        }
+        return OutputsValue;
+    }
+
+    public List<TransactionInput> getInputs(){
+        return inputs;
+    }
+
+    public List<TransactionOutput> getOutputs(){
+        return outputs;
+    }
+
+    public Double getValue(){
+        return value;
     }
 
     
